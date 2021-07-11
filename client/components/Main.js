deb22b95b23acd781fc588da1831003839bed1ef
Ava Choi <alexbchoi@gmail.com>
2021-07-10 21:16:33 -0400

    cycle2

--- client/components/Main.js
+++ client/components/Main.js
@@ -1,24 +1,23 @@
-import React from 'react';
-import {HashRouter as Router, Switch, Route} from 'react-router-dom'
-import StudentList from './StudentList.js';
-import SingleStudent from './SingleStudent';
+import React from "react";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
+import StudentList from "./StudentList.js";
+import SingleStudent from "./SingleStudent";
 
 export default class Main extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+	constructor(props) {
+		super(props);
+	}
 
-  render() {
-    return (
-      <Router>
-        <div>
-          <h1>Student Dashboard</h1>
-        </div>
-        <Switch>
-          <Route exact path="/" component={StudentList} />
-          {/* add your Route here! */}
-        </Switch>
-      </Router>
-    );
-  }
+	render() {
+		return (
+			<Router>
+				<div>
+					<h1>Student Dashboard</h1>
+				</div>
+				<Switch>
+					<Route exact path="/" component={StudentList} />
+				</Switch>
+			</Router>
+		);
+	}
 }
--- client/components/StudentList.js
+++ client/components/StudentList.js
@@ -1,40 +1,39 @@
-import React from 'react';
-import {fetchStudents} from '../redux/store';
-import {connect} from 'react-redux'
-import {Link} from 'react-router-dom'
+import React from "react";
+import { fetchStudents } from "../redux/store";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
 
 class StudentList extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+	constructor(props) {
+		super(props);
+	}
 
-  componentDidMount() {
-    this.props.loadStudents();
-  }
+	componentDidMount() {
+		this.props.loadStudents();
+	}
 
-  render() {
-    return (
-      <ul>
-        {this.props.students.map((student) => (
-          <li key={student.id}>
-            <div>
-              <p>Name: {student.fullName}</p>
-              <p>Email: {student.email}</p>
-            </div>
-          </li>
-        ))}
-      </ul>
-    )
-
-  }
+	render() {
+		return (
+			<ul>
+				{this.props.students.map((student) => (
+					<li key={student.id}>
+						<div>
+							<p>Name: {student.fullName}</p>
+							<p>Email: {student.email}</p>
+						</div>
+					</li>
+				))}
+			</ul>
+		);
+	}
 }
 
 const mapStateToProps = (state) => ({
-  students: state.students
+	students: state.students,
 });
 
 const mapDispatchToProps = (dispatch) => ({
-  loadStudents: () => dispatch(fetchStudents())
-})
+	loadStudents: () => dispatch(fetchStudents()),
+});
 
 export default connect(mapStateToProps, mapDispatchToProps)(StudentList);
