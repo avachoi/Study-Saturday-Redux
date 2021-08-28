5b8847583f24c79f597fa4ca4ceff8695e446469
Ava Choi <alexbchoi@gmail.com>
2021-08-28 14:16:12 -0400

    cycle3

--- client/components/SingleStudent.js
+++ client/components/SingleStudent.js
@@ -1,78 +1,97 @@
-import React from 'react';
+import React from "react";
+import { fetchAStudent } from "../redux/store";
+import { connect } from "react-redux";
 
 const avgGrade = (tests) => {
-  return Math.round(
-    tests.map((test) => test.grade).reduce((x, y) => x + y) / tests.length
-  );
+	return Math.round(
+		tests.map((test) => test.grade).reduce((x, y) => x + y) / tests.length
+	);
 };
 
 const DUMMY_DATA = {
-  id: 1,
-  fullName: "Student McDummydata",
-  firstName: "Student",
-  lastName: "McDummydata",
-  email: "sm@dummydata.com",
-  tests: [
-    {
-      id: 1,
-      subject: "Computer Science",
-      grade: 45,
-    },
-    {
-      id: 6,
-      subject: "Art",
-      grade: 60,
-    },
-    {
-      id: 12,
-      subject: "ullam",
-      grade: 45,
-    },
-  ],
+	id: 1,
+	fullName: "Student McDummydata",
+	firstName: "Student",
+	lastName: "McDummydata",
+	email: "sm@dummydata.com",
+	tests: [
+		{
+			id: 1,
+			subject: "Computer Science",
+			grade: 45,
+		},
+		{
+			id: 6,
+			subject: "Art",
+			grade: 60,
+		},
+		{
+			id: 12,
+			subject: "ullam",
+			grade: 45,
+		},
+	],
 };
 
 class SingleStudent extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+	constructor(props) {
+		super(props);
+	}
 
-  render() {
-    const student = DUMMY_DATA;
-    const hasTests = student.tests.length;
+	async componentDidMount() {
+		await this.props.loadAStudent(this.props.match.params.id);
+	}
+	render() {
+		console.log("props", this.props);
+		const student = this.props.student;
+		const hasTests = student.tests;
+		// const hasTests = student.tests && student.tests.length;;
 
-    return (
-      <div>
-        <h3>Detail: {student.fullName}</h3>
-        {hasTests ? (
-          <React.Fragment>
-            <h3>Average grade: {avgGrade(student.tests)}%</h3>
-            <div>
-              <table>
-                <thead>
-                  <tr>
-                    <th>Subject</th>
-                    <th>Grade</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {student.tests.map((test) => {
-                    return (
-                      <tr key={test.id}>
-                        <td>{test.subject}</td>
-                        <td>{test.grade}%</td>
-                      </tr>
-                    );
-                  })}
-                </tbody>
-              </table>
-            </div>
-          </React.Fragment>
-        ) : (
-          <h4>No tests on record.</h4>
-        )}
-      </div>
-    );
-  }
+		return (
+			<div>
+				<h3>Detail: {student.fullName}</h3>
+				{hasTests ? (
+					<React.Fragment>
+						<h3>Average grade: {avgGrade(student.tests)}%</h3>
+						<div>
+							<table>
+								<thead>
+									<tr>
+										<th>Subject</th>
+										<th>Grade</th>
+									</tr>
+								</thead>
+								<tbody>
+									{student.tests.map((test) => {
+										return (
+											<tr key={test.id}>
+												<td>{test.subject}</td>
+												<td>{test.grade}%</td>
+											</tr>
+										);
+									})}
+								</tbody>
+							</table>
+						</div>
+					</React.Fragment>
+				) : (
+					<h4>No tests on record.</h4>
+				)}
+			</div>
+		);
+	}
+}
+
+const mapStateToProps = (state) => {
+	return {
+		student: state.student,
+	};
 };
 
-export default SingleStudent;
+const mapDispatchToProps = (dispatch) => {
+	return {
+		loadAStudent: (id) => dispatch(fetchAStudent(id)),
+	};
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(SingleStudent);
--- client/components/StudentList.js
+++ client/components/StudentList.js
@@ -1,41 +1,42 @@
-import React from 'react';
-import {fetchStudents} from '../redux/store';
-import {connect} from 'react-redux'
-import {Link} from 'react-router-dom'
+import React from "react";
+import { fetchStudents } from "../redux/store";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
 
 class StudentList extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+	constructor(props) {
+		super(props);
+	}
 
-  componentDidMount() {
-    this.props.loadStudents();
-  }
+	componentDidMount() {
+		this.props.loadStudents();
+	}
 
-  render() {
-    return (
-      <ul>
-        {this.props.students.map((student) => (
-          <li key={student.id}>
-            <div>
-              <p>Name: {student.fullName}</p>
-              <p>Email: {student.email}</p>
-              <Link to={`/students/${student.id}`}>View Detail</Link>
-            </div>
-          </li>
-        ))}
-      </ul>
-    )
-
-  }
+	render() {
+		return (
+			<ul>
+				{this.props.students.map((student) => (
+					<li key={student.id}>
+						<div>
+							<p>Name: {student.fullName}</p>
+							<p>Email: {student.email}</p>
+							<Link to={`/students/${student.id}`} studentId={student.id}>
+								View Detail
+							</Link>
+						</div>
+					</li>
+				))}
+			</ul>
+		);
+	}
 }
 
 const mapStateToProps = (state) => ({
-  students: state.students
+	students: state.students,
 });
 
 const mapDispatchToProps = (dispatch) => ({
-  loadStudents: () => dispatch(fetchStudents())
-})
+	loadStudents: () => dispatch(fetchStudents()),
+});
 
 export default connect(mapStateToProps, mapDispatchToProps)(StudentList);
